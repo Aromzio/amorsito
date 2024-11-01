1252f2e2c4a087642c923d99795f7cc21806683a
Aromzio <aron231104@gmail.com>
2024-11-01 04:35:01 -0300

    TODO

---
+++ app/componentes/layout.jsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <div>
+            <div className="">
+               <h1>¡¡Feliz Aniversario Mi Amor!!</h1> 
+            </div>
+
+        </div>
+    )
+}
